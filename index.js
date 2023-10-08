1148743703995ccb7c9c4b27e0c737042d00e4d7
javier <javier@hodl.nl>
2023-10-08 22:09:29 +0200

    Multiple folders and video creation

--- index.js
+++ index.js
@@ -4,45 +4,51 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-    const images = [
-        {
-            path: './images/test1.png',
-            caption: 'Image One'
-        },
-        {
-            path: './images/test2.png',
-            caption: 'Image One'
-        },
-    ]
+    const SIGNS = [ 'aries', 'scorpio']
 
-    const videoOptions = {
-        fps: 25,
-        loop: 5, // seconds
-        transition: true,
-        transitionDuration: 1, // seconds
-        videoBitrate: 1024,
-        videoCodec: 'libx264',
-        size: '640x?',
-        audioBitrate: '128k',
-        audioChannels: 2,
-        format: 'mp4',
-        pixelFormat: 'yuv420p'
-    }
+    SIGNS.forEach(sign => {
+        // 1.- Call day horoscope 
+        const images = [
+            {
+                path: `./images/${sign}/1.jpeg`,
+                caption: `Image One is sign ${sign} ble ble ble`
+            },
+            {
+                path: `./images/${sign}/2.jpeg`,
+                caption: `Image One is sign ${sign} bla bla bla`
+            },
+        ]
 
-    videoshow(images, videoOptions)
-    .audio('./audio/1.mp3')
-    .save('scorpio.mp4')
-    .on('start', function (command) {
-        console.log('ffmpeg process started:', command)
-    })
-    .on('error', function (err, stdout, stderr) {
-        console.error('Error:', err)
-        console.error('ffmpeg stderr:', stderr)
-    })
-    .on('end', function (output) {
-        console.error('Video created in:', output)
-        res.json({ res: output })
-    })
+        const videoOptions = {
+            fps: 25,
+            loop: 5, // seconds
+            transition: true,
+            transitionDuration: 1, // seconds
+            videoBitrate: 1024,
+            videoCodec: 'libx264',
+            size: '640x?',
+            audioBitrate: '128k',
+            audioChannels: 2,
+            format: 'mp4',
+            pixelFormat: 'yuv420p'
+        }
+
+        videoshow(images, videoOptions)
+        .audio('./audio/1.mp3')
+        .save(`${sign}.mp4`)
+        .on('start', function (command) {
+            console.log('ffmpeg process started:', command)
+        })
+        .on('error', function (err, stdout, stderr) {
+            console.error('Error:', err)
+            console.error('ffmpeg stderr:', stderr)
+        })
+        .on('end', function (output) {
+            console.error('Video created in:', output)
+            console.log(output)
+        })
+    });
+    res.json({ response: "Videos created succesfully!" })
 })
 
 app.listen(port, () => {
